111ee4ba43e572953ceaf7f8626f57b768e21b8b
michal.koscak@gmail.com <michal.koscak@gmail.com@a3f97af9-5a5a-8481-7e3c-570d527bf882>
2013-10-23 18:44:41 +0000

    Prvotny commit.

---
+++ trunk/Koberce_2/FrmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Koberce_2
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.toolbarMain = new System.Windows.Forms.ToolStrip();
+            this.btnToolExit = new System.Windows.Forms.ToolStripButton();
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.toolbarMain.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolbarMain
+            // 
+            this.toolbarMain.BackColor = System.Drawing.SystemColors.Control;
+            this.toolbarMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolbarMain.ImageScalingSize = new System.Drawing.Size(48, 48);
+            this.toolbarMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnToolExit});
+            this.toolbarMain.Location = new System.Drawing.Point(0, 0);
+            this.toolbarMain.Name = "toolbarMain";
+            this.toolbarMain.Size = new System.Drawing.Size(1138, 55);
+            this.toolbarMain.TabIndex = 0;
+            this.toolbarMain.Text = "Toolbar";
+            // 
+            // btnToolExit
+            // 
+            this.btnToolExit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnToolExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnToolExit.Image = ((System.Drawing.Image)(resources.GetObject("btnToolExit.Image")));
+            this.btnToolExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnToolExit.Name = "btnToolExit";
+            this.btnToolExit.Size = new System.Drawing.Size(52, 52);
+            this.btnToolExit.Text = "Exit application";
+            this.btnToolExit.Click += new System.EventHandler(this.btnToolExit_Click);
+            // 
+            // tabMain
+            // 
+            this.tabMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabMain.Controls.Add(this.tabPage1);
+            this.tabMain.Location = new System.Drawing.Point(12, 58);
+            this.tabMain.Multiline = true;
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(1114, 620);
+            this.tabMain.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1106, 594);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 690);
+            this.Controls.Add(this.tabMain);
+            this.Controls.Add(this.toolbarMain);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(740, 465);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Global - part II - v0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.toolbarMain.ResumeLayout(false);
+            this.toolbarMain.PerformLayout();
+            this.tabMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolbarMain;
+        private System.Windows.Forms.ToolStripButton btnToolExit;
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
