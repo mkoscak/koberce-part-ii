ce652d620307c597d758fb179c1af80da4548301
michal.koscak@gmail.com <michal.koscak@gmail.com@a3f97af9-5a5a-8481-7e3c-570d527bf882>
2013-11-17 15:32:23 +0000

    Pridane referencie na epplus, sqlite. IPresenter interface, DBHelper control a system pridavania dalsich uc.

---
+++ trunk/Koberce_2/DBProvider.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Koberce_2
+{
+    public class DBProvider
+    {
+        public static string DataSource
+        {
+            get
+            {
+                return @".\global.db";
+            }
+        }
+
+        public DBProvider()
+        {
+            // vytvorenie DB suboru
+            if (!File.Exists(DataSource))
+            {
+                SQLiteConnection.CreateFile(DataSource);
+            }
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            try
+            {
+                var sql_con = new SQLiteConnection(@"Data Source=" + DataSource + "; Version=3;");
+
+                return sql_con;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(null, "Error while creating DB connection!", "GetConnection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return null;
+        }
+
+        public void ExecuteNonQuery(string txtQuery)
+        {
+            using (var sql_con = GetConnection())
+            {
+                sql_con.Open();
+                var sql_cmd = sql_con.CreateCommand();
+                sql_cmd.CommandText = txtQuery;
+                sql_cmd.ExecuteNonQuery();
+                sql_con.Close();
+            }
+        }
+
+        public DataSet ExecuteQuery(string query)
+        {
+            DataSet DS = new DataSet();
+
+            using (var sql_con = GetConnection())
+            {
+                sql_con.Open();
+                var sql_cmd = sql_con.CreateCommand();
+                var DB = new SQLiteDataAdapter(query, sql_con);
+                DS.Reset();
+                DB.Fill(DS);
+                sql_con.Close();
+            }
+            return DS;
+        }
+
+        public DataSet ExecuteQuery(string tableName, string where, string order)
+        {
+            DataSet DS = new DataSet();
+
+            using (var sql_con = GetConnection())
+            {
+                sql_con.Open();
+                var sql_cmd = sql_con.CreateCommand();
+                string CommandText = "select * from " + tableName + " A " + where + " " + order;
+                var DB = new SQLiteDataAdapter(CommandText, sql_con);
+                DS.Reset();
+                DB.Fill(DS);
+                sql_con.Close();
+            }
+            return DS;
+        }
+
+        static string DBPrice(double price)
+        {
+            return price.ToString().Replace(',','.');
+        }
+    }
+}
---
+++ trunk/Koberce_2/ucDBHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Koberce_2
+{
+    public partial class ucDBHelper : UserControl
+    {
+        DBProvider db;
+
+        public ucDBHelper(DBProvider db)
+        {
+            InitializeComponent();
+
+            this.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+
+            this.db = db;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNonQueryRes.Clear();
+            txtQuery.Clear();
+        }
+
+        private void btnExecQuery_Click(object sender, EventArgs e)
+        {
+            var q = txtQuery.SelectedText;
+            try
+            {
+                var ds = db.ExecuteQuery(q);
+                if (ds != null && ds.Tables.Count > 0)
+                    gridDBres.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                txtNonQueryRes.AppendText(Environment.NewLine + ex.ToString());
+            }
+        }
+
+        private void btnExecNonQuery_Click(object sender, EventArgs e)
+        {
+            var q = txtQuery.SelectedText;
+            try
+            {
+                db.ExecuteNonQuery(q);
+                txtNonQueryRes.AppendText(Environment.NewLine + "Exec successful!");
+            }
+            catch (Exception ex)
+            {
+                txtNonQueryRes.AppendText(Environment.NewLine + ex.ToString());
+            }
+        }
+    }
+}
