e16ab6c6f1d29d498b3ba4b8982a4b41fc337d6c
michal.koscak <michal.koscak@gmail.com>
2013-11-21 19:50:18 +0000

    Codelist class.

---
+++ Koberce_2/CodeList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Koberce_2
+{
+    /// <summary>
+    /// Help class as codelist
+    /// </summary>
+    public class CodeList
+    {
+        public long id;
+        public string name;
+
+        public CodeList(long id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
