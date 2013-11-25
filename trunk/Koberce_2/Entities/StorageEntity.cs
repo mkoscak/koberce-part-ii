fca9d135bbf2b2df715551c2c881b26f67158800
michal.koscak@gmail.com <michal.koscak@gmail.com@a3f97af9-5a5a-8481-7e3c-570d527bf882>
2013-11-25 17:01:05 +0000

    UC storages added.

--- trunk/Koberce_2/Entities/StorageEntity.cs
+++ trunk/Koberce_2/Entities/StorageEntity.cs
@@ -5,24 +5,22 @@ using System.Text;
 
 namespace Koberce_2.Entities
 {
-    class SupplierEntity : BaseEntity<SupplierEntity>
+    class StorageEntity : BaseEntity<StorageEntity>
     {
         public string Name { get; set; }
         public string Address { get; set; }
         public string Address2 { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public long? NrSerieId { get; set; }
 
         static string NAME = "NAME";
         static string ADDRESS = "ADDRESS";
         static string ADDRESS2 = "ADDRESS2";
         static string PHONE = "PHONE";
         static string EMAIL = "EMAIL";
-        static string NR_SERIE_ID = "NR_SERIE_ID";
 
-        public SupplierEntity()
-            : base(DBProvider.T_SUPPLIER)
+        public StorageEntity()
+            : base(DBProvider.T_STORAGE)
         {
             Clear();
         }
@@ -36,17 +34,16 @@ namespace Koberce_2.Entities
             Address2 = string.Empty;
             Phone = string.Empty;
             Email = string.Empty;
-            NrSerieId = null;
         }
 
-        public static List<SupplierEntity> LoadAll()
+        public static List<StorageEntity> LoadAll()
         {
-            var ret = new List<SupplierEntity>();
-            var table = LoadAllValidData(DBProvider.T_SUPPLIER);
+            var ret = new List<StorageEntity>();
+            var table = LoadAllValidData(DBProvider.T_STORAGE);
 
             for (int i = 0; i < table.Rows.Count; i++)
             {
-                var toAdd = new SupplierEntity();
+                var toAdd = new StorageEntity();
                 toAdd.ParseFromRow(table.Rows[i]);
 
                 ret.Add(toAdd);
@@ -68,9 +65,9 @@ namespace Koberce_2.Entities
 
         public void Save()
         {
-            Save(string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", ID, NAME, ADDRESS, ADDRESS2, PHONE, EMAIL, NR_SERIE_ID, COMMENT, VALID),
-                string.Format("{0},\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",{6},\"{7}\",{8}",
-                Common.NullableLong(Id), Name, Address, Address2, Phone, Email, Common.NullableLong(NrSerieId), Comment, Valid ? 1 : 0
+            Save(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", ID, NAME, ADDRESS, ADDRESS2, PHONE, EMAIL, COMMENT, VALID),
+                string.Format("{0},\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",{7}",
+                Common.NullableLong(Id), Name, Address, Address2, Phone, Email, Comment, Valid ? 1 : 0
                 ));
         }
 
@@ -83,7 +80,6 @@ namespace Koberce_2.Entities
             Address2 = row[ADDRESS2].ToString();
             Phone = row[PHONE].ToString();
             Email = row[EMAIL].ToString();
-            NrSerieId = long.Parse(row[NR_SERIE_ID].ToString());
         }
 
         public override string ToString()
