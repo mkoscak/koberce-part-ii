8e16effe4e3bcea0e3714a0b6f5bc67a08610dd1
michal.koscak@gmail.com <michal.koscak@gmail.com@a3f97af9-5a5a-8481-7e3c-570d527bf882>
2013-11-25 17:11:39 +0000

    Minor changes.

--- trunk/Koberce_2/Common.cs
+++ trunk/Koberce_2/Common.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 namespace Koberce_2
 {
@@ -10,6 +11,44 @@ namespace Koberce_2
         public static string NullableLong(long? l)
         {
             return l.HasValue ? l.Value.ToString() : "null";
+        }
+
+        public static double GetPrice(string strPrice)
+        {
+            if (strPrice == null)
+                return double.NaN;
+
+            var tmp = CleanPrice(strPrice);
+
+            double ret = double.NaN;
+            try
+            {
+                ret = double.Parse(tmp);
+            }
+            catch (Exception)
+            {
+            }
+
+            return ret;
+        }
+
+        public static string CleanPrice(string strPrice)
+        {
+            // cena obsahuje aj bodku aj ciarku, napr 1,000.25.. prvy znak vyhodime
+            if (strPrice.Contains(',') && strPrice.Contains('.'))
+            {
+                int pointIndex = strPrice.LastIndexOf('.');
+                int commaIndex = strPrice.LastIndexOf(',');
+
+                if (pointIndex > commaIndex)
+                    strPrice = strPrice.Replace(",", "");   // odstranime vsetky ciarky
+                else
+                    strPrice = strPrice.Replace(".", "");   // odstranime vsetky bodky
+            }
+
+            strPrice = new string(strPrice.ToCharArray().Where(c => Char.IsDigit(c) || c == ',' || c == '.' || c == '-').ToArray()).Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator).Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+            return strPrice;
         }
     }
 }
